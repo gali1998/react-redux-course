b451d18cbd98f45529474f8a8d9628be31915609
Gali <gali1998@gmail.com>
2019-03-28 10:17:17 +0200

    async in redux

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,23 @@
 import * as types from "./actionTypes";
+import * as coursesApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return coursesApi
+      .getCourses()
+      .then(courses => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
